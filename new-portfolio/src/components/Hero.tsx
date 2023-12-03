51617f3dfa595af592fabf23d26b597228652643
CrimsonCoder42 <nostro37@gmail.com>
2023-12-03 14:09:06 -0500

    max size pic

--- new-portfolio/src/components/Hero.tsx
+++ new-portfolio/src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { portpic, portpic1, portpic2 } from '../assets';
+import { portpic2 } from '../assets'; // Assuming this is the image you want to use
 import { textVariant, zoomIn, staggerContainer } from '../utils/motion';
 
 const Hero = () => {
@@ -11,43 +11,47 @@ const Hero = () => {
 
   return (
     <motion.section
-      className="relative flex items-center justify-center w-full h-screen overflow-hidden"
+      className="relative w-full h-screen overflow-hidden"
       variants={containerVariants}
       initial="hidden"
       animate="show"
     >
-      <div className="flex flex-col md:flex-row items-center justify-center gap-30 md:gap-35 lg:gap-40 px-6 md:px-16 w-full h-full max-w-7xl">
-        
-        <motion.div className='flex justify-center items-center mb-5 md:mb-0' variants={imageVariants}>
-          <motion.img 
-            src={portpic2}
-            alt="Profile Picture"
-            className='w-60 h-70 md:w-50 md:h-70 lg:w-80 lg:h-90 '
-            variants={imageVariants}
-          />
-        </motion.div>
+      <motion.div 
+        className="absolute inset-0 flex flex-col justify-center px-6 md:px-16"
+        variants={titleVariants}
+      >
+        <motion.h1
+          className="text-4xl md:text-4xl lg:text-5xl xl:text-6xl font-black leading-none text-white z-10"
+          variants={titleVariants}
+        >
+          Hi, I'm <span className="text-orange-500">Devin</span>
+        </motion.h1>
+        <motion.p
+          className="text-lg md:text-lg lg:text-xl leading-tight text-white z-10"
+          variants={subtitleVariants}
+        >
+          I engineer sleek, web solutions.
+        </motion.p>
+      </motion.div>
 
-        <motion.div className="flex flex-col items-center justify-center" variants={titleVariants}>
-          <motion.h1
-            className="text-4xl md:text-4xl lg:text-5xl xl:text-6xl font-black leading-none text-white"
-            variants={titleVariants}
-          >
-            Hi, I'm <span className="text-orange-500">Devin</span>
-          </motion.h1>
-          <motion.p
-            className="text-lg md:text-lg lg:text-xl leading-tight text-white"
-            variants={subtitleVariants}
-          >
-            I engineer sleek, web solutions.
-          </motion.p>
-        </motion.div>
-
-      </div>
+      <motion.div 
+        className='absolute bottom-0 left-0 right-0 flex justify-center items-end z-0'
+        variants={imageVariants}
+      >
+        <motion.img 
+          src={portpic2}
+          alt="Profile Picture"
+          className='w-1/3 md:w-1/2 md:max-w-none h-auto md:h-70 lg:h-90 object-cover'
+          variants={imageVariants}
+        />
+      </motion.div>
     </motion.section>
   );
 };
 
 export default Hero;
+
+
 
 
 
