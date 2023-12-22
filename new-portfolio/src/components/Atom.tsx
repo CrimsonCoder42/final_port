6d0b5a1d0f260232212f89f395a98be851498994
CrimsonCoder42 <nostro37@gmail.com>
2023-12-22 13:49:07 -0500

    move canvas into comp.

---
+++ new-portfolio/src/components/Atom.tsx
@@ -0,0 +1,13 @@
+
+import React, { useMemo } from "react";
+import { Line, Sphere } from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import * as THREE from "three";
+
+const AtomShape = () => {
+  // ... Atom component code ...
+};
+
+export default AtomShape;
+
+// Other shapes (like SphereShape, CubeShape, etc.)
--- new-portfolio/src/components/Cards.tsx
+++ new-portfolio/src/components/Cards.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { FC, useEffect, Suspense, useState } from 'react';
 import { motion, AnimatePresence } from "framer-motion";
-import { fadeIn } from "../utils/motion";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+
+import Cube from './Cube'; 
+import AtomShape from './Atom'; 
 
 interface ServiceCardProps {
   title: string;
@@ -8,20 +13,38 @@ interface ServiceCardProps {
   index: number;
   tech: string[];
 }
+                //add different shapes and check  the lighting of the canvas     
+const DisplayCard: React.FC<ServiceCardProps> = ({ title, icon, tech }) => {
 
-const DisplayCard: React.FC<ServiceCardProps> = ({ index, title, icon, tech }) => {
+  const renderShape = (icon) => {
+    switch(icon) {
+      case 'atom-icon':
+        return <AtomShape />;
+      // Add cases for other icons
+      default:
+        return <DefaultShape />;
+    }
+  };
+
   return (
     <>
       
         <div
           className='rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
-          <div className="w-1/2 md:w-1/3 lg:w-1/4 p-2">
-            <img
-              src={icon}
-              alt='web-development'
-              className='w-full h-auto object-contain'
-            />
+          <div className="justify-center ">
+            <Canvas 
+            className="z-10"
+            style={{ maxHeight: '150px', maxWidth: '150px' }}
+            camera={{ position: [20, 20, 20], fov: 50 }}
+          >
+            <Suspense fallback={null}>
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[3, 2, 1]} />
+              <Cube /> 
+              <OrbitControls enableZoom={false} autoRotate maxDistance={50} />
+            </Suspense>
+          </Canvas>
           </div>
 
           <h3 className='text-white text-[20px] font-bold text-center'>
@@ -36,7 +59,7 @@ const DisplayCard: React.FC<ServiceCardProps> = ({ index, title, icon, tech }) =
                   whileInView={{ opacity: 1, transition: { duration: 2 } }} // Scroll-triggered animation
                   initial={{ opacity: 0 }} // Initial state before scrolling into view
                   key={idx}
-                  className='w-1/3 md:w-1/4 lg:w-1/6 p-2'
+                  className='w-1/4 md:w-1/4 lg:w-1/6 p-2'
                 >
                   <img
                     src={item}
